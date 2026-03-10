3ea7b124e608e4691d2033b4095e792aa7f77c7a
huypq3 <huy28184@gmail.com>
2026-03-10 11:45:54 +0700

    check resize website

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
       </head>
       <body className={`${cormorant.variable} ${dmSans.variable}`}>{children}</body>
     </html>
